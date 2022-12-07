6f32a497f5e69affd5356cea403955e37d33d763
Finn Kitor <118993032+Finn-Kitor@users.noreply.github.com>
2022-12-07 15:56:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# area of a trapezoid
+#the program will ask the user about the area of a trapezoid
+print (" today, we will calculate the area of a trapezoid ")
+
+base1 = int(input("enter the base1 (cm): "))
+
+base2 = int(input("enter the base2 (cm): "))
+
+height = int(input("enter the height (cm): "))
+
+
+area = base1 * base2 / 2 * height
+
+print (" the area of the trapezoid with base1 " + str(base1) + " cm and base2 " + str(base2) + " cm and height " + str(height) + " cm is " + str(area) + " cm^2.")                  
